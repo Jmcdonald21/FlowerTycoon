a5009b983aa52d0d6c76ae3b5ca8b3ac03b756ff
Jmcdonald21 <57681338+Jmcdonald21@users.noreply.github.com>
2024-04-26 19:29:24 -0700

    Add files via upload

---
+++ FlowerDeliveryTycoon/CharacterHull.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
